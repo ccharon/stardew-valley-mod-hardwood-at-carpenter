e50945d2c4c46363d1628cd84004c98edc6c170c
ccharon <christian.charon@mailbox.org>
2018-11-05 00:46:17 +0100

    code cleanup

--- HardwoodAtCarpentersShop/HardwoodAtCarpentersShop.cs
+++ HardwoodAtCarpentersShop/HardwoodAtCarpentersShop.cs
@@ -4,54 +4,48 @@ using StardewValley;
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using StardewValley.Menus;
-using System;
 
 namespace HardwoodAtCarpentersShop
 {
     public class HardwoodAtCarpenterShop : Mod
     {
-        private const int woodItemId = 388;
-        private const int hardwoodItemId = 709;
-        // based on the price of wood, buy for 50, sell for 2, i choose 450 as hardwood sells for 15
-        private const int hardwoodPrice = 450;
+        const int woodItemId = 388;
+        const int hardwoodItemId = 709;
+        const int hardwoodPrice = 450;
+        const string fieldForSale = "forSale";
+        const string fieldItemPriceAndStock = "itemPriceAndStock";
+        const string shopKeeperRobin = "Robin";
 
         public override void Entry(IModHelper helper)
         {
             MenuEvents.MenuChanged += Events_MenuChanged;
         }
 
-        private void Events_MenuChanged(object sender, EventArgsClickableMenuChanged e)
+        void Events_MenuChanged(object sender, EventArgsClickableMenuChanged e)
         {
-            if (e.NewMenu is ShopMenu currentShopMenu)
+            if (IsCarpentersShopMenu(e.NewMenu))
             {
-                if (currentShopMenu.portraitPerson.Equals(StardewValley.Game1.getCharacterFromName("Robin", false)))
-                {
-                    Item hardwood = (Item) new StardewValley.Object(Vector2.Zero, hardwoodItemId, ShopMenu.infiniteStock);
+                ShopMenu carpentersShopMenu = (ShopMenu) e.NewMenu;
 
-                    IReflectedField<List<Item>> forSaleInformation = this.Helper.Reflection.GetField<List<Item>>(currentShopMenu, "forSale");
-                    List<Item> itemsForSale = forSaleInformation.GetValue();
-                    itemsForSale.Insert(GetWoodPosition(itemsForSale) + 1,hardwood);
-                    forSaleInformation.SetValue(itemsForSale);
+                Item hardwood = new Object(Vector2.Zero, hardwoodItemId, ShopMenu.infiniteStock);
 
-                    IReflectedField<Dictionary<Item, int[]>> inventoryInformation = this.Helper.Reflection.GetField<Dictionary<Item, int[]>>(currentShopMenu, "itemPriceAndStock");
-                    Dictionary<Item, int[]> itemPriceAndStock = inventoryInformation.GetValue();
-                    itemPriceAndStock.Add(hardwood, new int[2] { hardwoodPrice, ShopMenu.infiniteStock });
-                    inventoryInformation.SetValue(itemPriceAndStock);
-                }
+                IReflectedField<List<Item>> itemsForSale = Helper.Reflection.GetField<List<Item>>(carpentersShopMenu, fieldForSale);
+                itemsForSale.GetValue().Insert(GetWoodPosition(itemsForSale.GetValue()) + 1, hardwood);
+
+                IReflectedField<Dictionary<Item, int[]>> itemsInventory = Helper.Reflection.GetField<Dictionary<Item, int[]>>(carpentersShopMenu, fieldItemPriceAndStock);
+                itemsInventory.GetValue().Add(hardwood, new int[] { hardwoodPrice, ShopMenu.infiniteStock });
             }
         }
 
-        private int GetWoodPosition(List<Item> items) 
+        bool IsCarpentersShopMenu(IClickableMenu menu) 
         {
-            for (int i = 0; i < items.Count; i++)
-            {
-                if (items[i].parentSheetIndex == woodItemId)
-                {
-                    return i;
-                }
-            }
+            return (menu is ShopMenu shopMenu) && shopMenu.portraitPerson.Equals(Game1.getCharacterFromName(shopKeeperRobin, false));
+        }
 
-            return items.Count;
+        int GetWoodPosition(List<Item> items) 
+        {
+            int woodPosition = items.FindIndex(item => item.parentSheetIndex == woodItemId);
+            return (woodPosition >= 0) ? woodPosition : items.Capacity;
         }
     }
 }
