97b285aab59946c488a28a760b3895f3e601e3f0
Christian Charon <christian.charon@mailbox.org>
2021-04-01 00:20:33 +0200

    now works with stardew valley 1.5.4 SMAPI 3.9.5

--- HardwoodAtCarpentersShop/HardwoodAtCarpentersShop.cs
+++ HardwoodAtCarpentersShop/HardwoodAtCarpentersShop.cs
@@ -4,34 +4,36 @@ using StardewValley;
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using StardewValley.Menus;
+using Object = StardewValley.Object;
 
 namespace HardwoodAtCarpentersShop
-{
+{ 
     public class HardwoodAtCarpenterShop : Mod
     {
-        const int woodItemId = 388;
-        const int hardwoodItemId = 709;
-        const int hardwoodPrice = 450;
+        private const int WoodItemId = 388;
+        private const int HardwoodItemId = 709;
+        private const int HardwoodPrice = 450;
 
         public override void Entry(IModHelper helper)
         {
-            MenuEvents.MenuChanged += Events_MenuChanged;
+            helper.Events.Display.MenuChanged += OnMenuChanged;
         }
 
-        void Events_MenuChanged(object sender, EventArgsClickableMenuChanged e)
+        private void OnMenuChanged(object sender, MenuChangedEventArgs eventArgs)
         {
-            if (e.NewMenu is ShopMenu shopMenu && shopMenu.portraitPerson.Equals(Game1.getCharacterFromName("Robin", false)))
-            {
-                Item hardwoodItem = new Object(Vector2.Zero, hardwoodItemId, ShopMenu.infiniteStock);
+            if (!(eventArgs.NewMenu is ShopMenu shopMenu) || 
+                !shopMenu.portraitPerson.Equals(Game1.getCharacterFromName("Robin"))) return;
 
-                IReflectedField<List<Item>> itemsForSale = Helper.Reflection.GetField<List<Item>>(shopMenu, "forSale");
+            Item hardwoodItem = new Object(Vector2.Zero, HardwoodItemId, ShopMenu.infiniteStock);
 
-                int woodItemPosition = itemsForSale.GetValue().FindIndex(item => item.parentSheetIndex == woodItemId);
-                itemsForSale.GetValue().Insert(woodItemPosition + 1, hardwoodItem);
+            IReflectedField<List<ISalable>> forSaleInformation = Helper.Reflection.GetField<List<ISalable>>(shopMenu, "forSale");
+            List<ISalable> forSale = forSaleInformation.GetValue();
+            int woodItemPosition = forSale.FindIndex(salable => salable is Item item && item.parentSheetIndex.Equals(WoodItemId));
+            forSale.Insert(woodItemPosition + 1, hardwoodItem);
 
-                IReflectedField<Dictionary<Item, int[]>> itemsInventory = Helper.Reflection.GetField<Dictionary<Item, int[]>>(shopMenu, "itemPriceAndStock");
-                itemsInventory.GetValue().Add(hardwoodItem, new int[] { hardwoodPrice, ShopMenu.infiniteStock });
-            }
+            IReflectedField<Dictionary<ISalable, int[]>> inventoryInformation = Helper.Reflection.GetField<Dictionary<ISalable, int[]>>(shopMenu, "itemPriceAndStock");
+            Dictionary<ISalable, int[]> itemPriceAndStock = inventoryInformation.GetValue();
+            itemPriceAndStock.Add(hardwoodItem, new[] {HardwoodPrice, ShopMenu.infiniteStock});
         }
     }
 }
